70535d75731754a5de228b5add17036a119af889
codeworks <instructors@boisecodeworks.com>
2022-08-25 16:25:57 -0600

    first iteration done

--- getKeep/index.js
+++ getKeep/index.js
@@ -1,13 +1,18 @@
+const keep = require('./keepData.json')
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+    let specs = `&w=${Math.round(Math.random()*500)+400}&h=${Math.round(Math.random()*500)+400}&q=60`
+    const response = {
+        name: random(keep.names),
+        description: random(keep.descriptions),
+        img: random(keep.images) + specs
+    }
+    context.log('Keep data generated', response)
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: response
     };
+}
+
+function random(arr){
+    return arr[Math.floor(Math.random()*arr.length)]
 }
